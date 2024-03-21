7becfd023e0891d88bf5a6b9276959faf2ac1892
John DH. Park <john.dh.park@icloud.com>
2024-03-21 09:36:17 -0700

    Chapter 14 Remaining

--- ch_14/GenArrays.java
+++ ch_14/GenArrays.java
@@ -21,6 +21,12 @@ class GenArrays {
         Integer[] n = { 1, 2, 3, 4, 5 };
 
         Gen<Integer> iOb = new Gen<Integer>(50, n);
+
+        // Can't create an array of type-specific generic references.
+        // Gen<Integer>[] gens = new Gen<Integer>[10]; // Wrong!
+        //
+        // This is OK.
+        Gen<?> gens = new Gen<?>[10]; // OK
     }
 
 }
